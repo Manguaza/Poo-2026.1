1ccf1737ac6ebf2b782af77e8352f50aaa2c5c1a
Matheus Santiago <santiago.g@escolar.ifrn.edu.br>
2026-03-31 19:21:33 +0000

    indo para a aula

--- aula_04/ex02.java
+++ aula_04/ex02.java
@@ -1,14 +1,14 @@
 package aula_04;
 
 class viagem {
- public double distancia = "0";
- public double tempo = "0";
- public void setDistancia(double v) {
-    if (v>=0)
+ private double distancia = 0;
+ private double tempo = 0;
+ public void SetDistancia(double distancia) {
+    if 
+ }
+    public void setHoras(double tempo) {
     
- }   
-}
-
+ }
 
 public class ex02 {
     
