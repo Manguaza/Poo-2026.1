868ff965ef3b70c226adf0dd91239bc24f3c72b4
Matheus Santiago <santiago.g@escolar.ifrn.edu.br>
2026-03-31 20:07:30 +0000

    finalizado

--- aula_04/ex02.java
+++ aula_04/ex02.java
@@ -2,14 +2,42 @@ package aula_04;
 
 class viagem {
  private double distancia = 0;
- private double tempo = 0;
+ private double horas = 0;
+ private double minutos = 0;
  public void SetDistancia(double distancia) {
-    if 
+     this.distancia = distancia;
  }
-    public void setHoras(double tempo) {
+    public void setTempo(double tempo) {
+   this.tempo = tempo;
     
  }
-
+ public void setminutos(double minutos) {
+   this.minutos = minutos;
+ }
+    public double getDistancia() {
+        return distancia;
+    }
+    public double getTempo() {
+        return tempo;
+ }
+  public double getMinutos () {
+     return minutos;
+  }
+  
+  public double calcularVelocidadeMedia () {
+   double tempoTotalHoras = horas + (minutos/60.00);
+  }
+   return tempoTotalHoras / distancia;
+ }
 public class ex02 {
-    
+    public static void main(String[] args) {
+        viagem.setdistancia (150);
+        viagem.sethoras (3);
+        viagem.setminutos (55);
+    }
+    double velocidade = calcularVelocidadeMedia;
+
+    System.out.println("distancia " + getDistancia);
+    System.out.println("Tempogasto" + getTempo);
+    System.out.println("velocidade" + velocidade);
 }
