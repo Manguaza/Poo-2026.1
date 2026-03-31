7dd43ac7b725fdaccc517329afa861eb9ed378cb
Matheus Santiago <santiago.g@escolar.ifrn.edu.br>
2026-03-31 19:01:24 +0000

    ressonancia

---
+++ aula_04/ex02.java
@@ -0,0 +1,15 @@
+package aula_04;
+
+class viagem {
+ public double distancia = "0";
+ public double tempo = "0";
+ public void setDistancia(double v) {
+    if (v>=0)
+    
+ }   
+}
+
+
+public class ex02 {
+    
+}
