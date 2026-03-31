4ce7f24754f72ce374d81db585f3f16af7c00745
Matheus Santiago <santiago.g@escolar.ifrn.edu.br>
2026-03-31 17:05:38 +0000

    13

--- aula_04/ex01.java
+++ aula_04/ex01.java
@@ -4,14 +4,28 @@ class Triangulo {
     private double b = 0;
     private double h = 0;
     public void setbase (double v) {
-        if (v >= 0) this.
+        if (v >= 0) this.b = v;
+        else throw new IllegalArgumentException("valor não pode ser negativo");
+    }
+    public void setAltura(double v) {
+        if (v >= 0) this.h = v;
+        else throw new IllegalArgumentExpection("valor não pode ser negativo");
+    }    
+    public double getbase(){
+        return this.b;
+    }
+    public double calcArea(){
+        return this.b * this.h / 2
     }
 }
 
 
 
-
-
 public class ex01 {
-    
+    public static void main(String[] args) {
+        Triangulo x =  new Triangulo();
+        x.setbase(10);
+        x.setAltura(20);
+        System.out.println(x.calcArea());
+    }
 }
